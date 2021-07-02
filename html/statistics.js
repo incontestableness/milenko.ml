7e96d7ac5d8f84e0780048a54a4002d97f5d8fa1
incontestableness <whyistherumg0ne@protonmail.com>
2021-07-02 10:28:06 -0500

    Render site graph properly and simplify code

--- html/statistics.js
+++ html/statistics.js
@@ -1,22 +1,25 @@
+// How often to request data from the GLaDOS API (data is cached for five seconds)
+var update_frequency = 5;
+// Maximum hours of data to display before pruning old data
+var hours = 2;
+
 // Graph color settings
 var total_color = "#A0A0A0"; // Gray
 var human_color = "#00FF6D"; // Green
 var malicious_color = "#CA7000"; // Orange
 var diff_color = "#E0E000"; // Yellow
 
-// Graph context settings
-// Basically the "range" of the y-axis
-var all_context = 500;
-var bot_context = 50;
-var diff_context = 5;
-
-// How often to request data from the GLaDOS API (data is cached for five seconds)
-var update_frequency = 5;
-// Maximum hours of data to display before pruning old data
-var hours = 1;
-
-// Set chart display defaults
+// Line thickness
 Chart.defaults.elements.line.borderWidth = 1;
+// Point size
+var pointSize = 2;
+
+// All of our scales are y-axes
+Chart.defaults.scales.linear.axis = "y";
+// Start y-axes at zero
+Chart.defaults.scales.linear.min = 0;
+// Don't include weird max values in the y-axes
+Chart.defaults.scales.linear.ticks.includeBounds = false;
 
 // Create graph
 var canvas = document.getElementById("graph").getContext("2d");
@@ -29,6 +32,7 @@ var graph = new Chart(canvas, {
 				borderColor: total_color,
 				pointBackgroundColor: total_color,
 				pointBorderColor: total_color,
+				pointRadius: pointSize,
 				yAxisID: "all_players",
 			},
 			{
@@ -36,13 +40,15 @@ var graph = new Chart(canvas, {
 				borderColor: human_color,
 				pointBackgroundColor: human_color,
 				pointBorderColor: human_color,
-				yAxisID: "humans",
+				pointRadius: pointSize,
+				yAxisID: "all_players",
 			},
 			{
 				label: "Total malicious bots in-game",
 				borderColor: malicious_color,
 				pointBackgroundColor: malicious_color,
 				pointBorderColor: malicious_color,
+				pointRadius: pointSize,
 				yAxisID: "malicious_bots"
 			},
 			{
@@ -50,6 +56,7 @@ var graph = new Chart(canvas, {
 				borderColor: diff_color,
 				pointBackgroundColor: diff_color,
 				pointBorderColor: diff_color,
+				pointRadius: pointSize,
 				yAxisID: "difficulty"
 			}
 		]
@@ -58,39 +65,38 @@ var graph = new Chart(canvas, {
 		maintainAspectRatio: false,
 		scales: {
 			all_players: {
-				axis: "y",
 				position: "left",
 				ticks: {
-					display: false,
-					includeBounds: false
-				}
-			},
-			humans: {
-				axis: "y",
-				position: "left",
-				ticks: {
+					// Don't display tick values as e.g. 7,000.0000000000010
+					callback: function(value, index, values) {
+						// Strip decimal, convert str to int, convert int to comma string
+						return parseInt(value.toFixed(0)).toLocaleString();
+					},
 					color: human_color,
-					includeBounds: false
-				}
+					stepSize: 250
+				},
+				// Give the graph some headroom
+				suggestedMax: 10000
 			},
 			malicious_bots: {
-				axis: "y",
 				position: "right",
 				ticks: {
 					color: malicious_color,
-					includeBounds: false
-				}
+					stepSize: 25
+				},
+				suggestedMax: 1000
 			},
 			difficulty: {
-				axis: "y",
 				position: "right",
 				ticks: {
+					// Pad the percentages to two decimal places and add a percent sign
 					callback: function(value, index, values) {
 						return value.toFixed(2) + "%";
 					},
 					color: diff_color,
-					includeBounds: false
-				}
+					stepSize: 0.25
+				},
+				suggestedMax: 10
 			}
 		}
 	}
@@ -131,58 +137,12 @@ function updateGraph() {
 				prune();
 			}
 
-			// Try to give meaningful numerical context to the y-axes
-			// This code is cursed
-			var all_max = Math.max.apply(Math, graph.data.datasets[0].data.slice(1));
-			var all_min = Math.max(Math.min.apply(Math, graph.data.datasets[1].data.slice(1)), 0);
-			var all_ctx = (all_context - (all_max - all_min) / 2);
-			// If the difference between the maximum and minimum value is less than the scale,
-			// _ctx should just be set to _context / 2
-			if (all_max - all_min < all_context) {
-				all_ctx = all_context / 2
-			}
-			var new_max = Math.max(all_max, all_max + all_ctx);
-			var new_min = Math.min(all_min, all_min - all_ctx);
-			//console.log("all: " + (new_max - new_min));
-			graph.options.scales.all_players.max = new_max;
-			graph.options.scales.all_players.min = new_min;
-			graph.options.scales.humans.max = new_max;
-			graph.options.scales.humans.min = new_min;
-
-			var bot_max = Math.max.apply(Math, graph.data.datasets[2].data.slice(1));
-			var bot_min = Math.max(Math.min.apply(Math, graph.data.datasets[2].data.slice(1)), 0);
-			var bot_ctx = (bot_context - (bot_max - bot_min) / 2);
-			// If the difference between the maximum and minimum value is less than the scale,
-			// _ctx should just be set to _context / 2
-			if (bot_max - bot_min < bot_context) {
-				bot_ctx = bot_context / 2
-			}
-			var new_max = Math.max(bot_max, bot_max + bot_ctx);
-			var new_min = Math.min(bot_min, bot_min - bot_ctx);
-			//console.log("bot: " + (new_max - new_min));
-			graph.options.scales.malicious_bots.max = new_max;
-			graph.options.scales.malicious_bots.min = new_min;
-
-			var diff_max = Math.max.apply(Math, graph.data.datasets[3].data.slice(1));
-			var diff_min = Math.max(Math.min.apply(Math, graph.data.datasets[3].data.slice(1)), 0);
-			var diff_ctx = (diff_context - (diff_max - diff_min) / 2);
-			// If the difference between the maximum and minimum value is less than the scale,
-			// _ctx should just be set to _context / 2
-			if (diff_max - diff_min < diff_context) {
-				diff_ctx = diff_context / 2
-			}
-			var new_max = Math.max(diff_max, diff_max + diff_ctx);
-			var new_min = Math.min(diff_min, diff_min - diff_ctx);
-			//console.log("diff: " + (new_max - new_min));
-			graph.options.scales.difficulty.max = new_max;
-			graph.options.scales.difficulty.min = new_min;
-
 			graph.update();
 		});
 	counter += 1;
 }
 
-alert("This graph will update every 5 seconds with live data from the GLaDOS API.\nYou can change which lines are shown on the graph by clicking each label at the top of the page.\nEach line's y-axis will automatically scale based on its min/max values, so do pay attention to them!\nBy default, old data will start getting pruned after an hour.");
+alert("This graph will update every 5 seconds with live data from the GLaDOS API.\nYou can change which lines are shown on the graph by clicking the labels at the top of the page.\nThe y-axes may automatically increase based on the current datasets, so do pay them some mind.\nBy default, old data will start getting pruned after two hours. You can change this behavior via window.hours in the console.");
 
 updateGraph();
 setInterval(updateGraph, update_frequency * 1000);
